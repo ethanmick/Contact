21a2f4672eeabda3046074ab3bd8efb82ea4adff
Ethan Mick <ethanmski@gmail.com>
2014-09-23 11:50:25 -0400

    Created a contact model object to hold contact info.

---
+++ Contact/Contact.swift
@@ -0,0 +1,21 @@
+//
+//  Contact.swift
+//  Contact
+//
+//  Created by Ethan Mick on 9/23/14.
+//  Copyright (c) 2014 Ethan Mick. All rights reserved.
+//
+
+import UIKit
+
+class Contact: NSObject {
+    
+    var firstName : String?
+    var lastName : String?
+    var phoneNumbers : [String: String]?
+    var addresses : [String: String]?
+    var image : UIImage?
+    var email : [String: String]?
+    var notes : String?
+    
+}
